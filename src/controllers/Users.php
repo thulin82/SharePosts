dbcf828795083c98159256084d00921aae061538
Markus Thulin <macky_b@hotmail.com>
2020-07-08 10:15:22 +0200

    Added User model with DB methods

--- src/controllers/Users.php
+++ src/controllers/Users.php
@@ -1,12 +1,65 @@
 <?php
     class Users extends Controller {
-        public function __construct(){
 
+        public function __construct(){
+            $this->userModel = $this->model('User');
         }
 
         public function register(){
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
                 // Proccess form
+                $data = [
+                    'name' => trim($_POST['name']),
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'confirm_password' => trim($_POST['confirm_password']),
+                    'name_err' => '',
+                    'email_err' => '',
+                    'password_err' => '',
+                    'confirm_password_err' => '',
+                ];
+
+                if (empty($data['name'])) {
+                    $data['name_err'] = 'Please enter a name';
+                }
+
+                if (empty($data['email'])) {
+                    $data['email_err'] = 'Please enter an email';
+                } else {
+                    if($this->userModel->findUserByEmail($data['email'])) {
+                        $data['email_err'] = 'Email already in system';
+                    }
+                }
+
+                if (empty($data['password'])) {
+                    $data['password_err'] = 'Please enter a password';
+                } elseif (strlen($data['password']) < 6) {
+                    $data['password_err'] = 'Password must be greater than 6 characters';
+                }
+
+                if (empty($data['confirm_password'])) {
+                    $data['confirm_password_err'] = 'Please enter a password';
+                } elseif ($data['password'] != $data['confirm_password']) {
+                    $data['confirm_password_err'] = 'Confirm password must match password';
+                }
+
+                if (empty($data['name_err']) && empty($data['email_err']) && empty($data['password_err']) && empty($data['confirm_password_err'])) {
+                    // Validated
+                    $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+
+                    if($this->userModel->registerUser($data)){
+                        flash('register_success', 'You are registered and can log in');
+                        redirect('users/login');
+                    } else {
+                        die('Something went wrong');
+                    }
+
+                } else {
+                    // Error, stay on view
+                    $this->view('users/register', $data);
+                }
+
             } else {
                 $data = [
                     'name' => '',
@@ -25,7 +78,29 @@
 
         public function login(){
             if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
                 // Proccess form
+                $data = [
+                    'email' => trim($_POST['email']),
+                    'password' => trim($_POST['password']),
+                    'email_err' => '',
+                    'password_err' => '',
+                ];
+
+                if (empty($data['email'])) {
+                    $data['email_err'] = 'Please enter an email';
+                }
+                if (empty($data['password'])) {
+                    $data['password_err'] = 'Please enter a password';
+                }
+                if (empty($data['email_err']) && empty($data['password_err'])) {
+                    // Validated
+                    die('SUCCESS');
+                } else {
+                    $this->view('users/login', $data);
+                }
+
+
             } else {
                 $data = [
                     'email' => '',
---
+++ src/models/User.php
@@ -0,0 +1,34 @@
+<?php
+    class User {
+        private $db;
+
+        public function __construct() {
+            $this->db = new Database;
+        }
+
+        public function findUserByEmail($email){
+            $this->db->query("SELECT * FROM users WHERE email = :email");
+            $this->db->bind(':email', $email);
+            $row = $this->db->single();
+
+            if ($this->db->rowCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+
+        }
+
+        public function registerUser($data){
+            $this->db->query('INSERT INTO users (name, email, password) VALUES (:name, :email, :password)');
+            $this->db->bind(':name', $data['name']);
+            $this->db->bind(':email', $data['email']);
+            $this->db->bind(':password', $data['password']);
+
+            if($this->db->execute()){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
